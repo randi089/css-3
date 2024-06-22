4c8fa1056b11630b2af655729b0417ca5f157ce5
randi089 <randifebriadi@gmail.com>
2024-06-22 08:45:27 +0700

    initial commit

---
+++ Gallery/js/script.js
@@ -0,0 +1,17 @@
+// const tes = document.querySelector('.tes');
+// const a = document.querySelectorAll('a');
+
+// a.forEach(function(e) {
+//     e.addEventListener('click', function (t) {
+//         t.preventDefault();
+//         let child = tes.lastElementChild;
+
+//         while (child) {
+//             tes.removeChild(child);
+            
+//             child = tes.lastElementChild;
+//         }
+
+//         tes.innerHTML = "<h1>Hello World!</h1>"
+//     });
+// });
